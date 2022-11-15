421f3eb38c912c0834429c9a470f69bf7a7feac7
rajavi20 <rajavishah.20@gmail.com>
2022-11-15 21:39:39 +0530

    Updates

---
+++ precache-manifest.2e2f0d88be9f1274186c3e62d635a89f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eabe44d56661d951d8e5",
+    "url": "/textutils/static/css/main.da32f9ff.chunk.css"
+  },
+  {
+    "revision": "eabe44d56661d951d8e5",
+    "url": "/textutils/static/js/main.eabe44d5.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/textutils/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/textutils/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "168bd853d713a82610a7",
+    "url": "/textutils/static/js/runtime~main.168bd853.js"
+  },
+  {
+    "revision": "bf5e0dc4efba3b5d7fadde57b8a762c9",
+    "url": "/textutils/index.html"
+  }
+];
